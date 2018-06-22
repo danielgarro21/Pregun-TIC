a0ffe4356c48eee250e7cc16bc9059298cde8c34
fernanda1810 <37128686+fernanda1810@users.noreply.github.com>
2018-06-21 22:45:49 -0600

    usuario finalizado
    funcionalidades, agregar,eliminar y modificar ya implementados en este documento

---
+++ preguntic/src/edu/ulatina/preguntic/servicio/Servicio.java
@@ -0,0 +1,27 @@
+
+package edu.ulatina.preguntic.servicio;
+
+import edu.ulatina.preguntic.objetos.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public abstract class Servicio{
+    protected String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    protected String DB_URL = "jdbc:mysql://localhost:3306/preguntic?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    //  Database credentials
+    protected String USER = "root";
+    protected String PASS = "1shotykillhead";
+    protected PreparedStatement paInsertar;
+    protected Connection conn = null;
+    protected Statement stmt = null;
+
+    
+    
+    
+    
+    public abstract void conectar();
+    
+    public abstract void accion();
+    }
+
