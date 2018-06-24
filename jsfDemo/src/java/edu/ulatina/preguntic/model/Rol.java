208505f7156c9bb5b848398fd1b8acd5306dc715
sgtxjosue <sgtxjosue@gmail.com>
2018-06-23 22:17:25 -0600

    traslado a web y log in hecho

---
+++ jsfDemo/src/java/edu/ulatina/preguntic/model/Rol.java
@@ -0,0 +1,50 @@
+
+package edu.ulatina.preguntic.model;
+
+
+public class Rol {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public Rol(int id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("=============================================================================\n");
+        sb.append("Nombre de rol: " + this.nombre);
+        sb.append("\n");
+        sb.append("Descripcion: " + this.descripcion);
+        sb.append("\n=============================================================================");
+        return sb.toString();
+    }
+}
---
+++ jsfDemo/src/java/edu/ulatina/preguntic/service/Servicio.java
@@ -0,0 +1,24 @@
+
+package edu.ulatina.preguntic.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+
+public abstract class Servicio {
+    protected String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    protected String DB_URL = "jdbc:mysql://localhost:3306/preguntic?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    //  Database credentials
+    protected String USER = "root";
+    protected String PASS = "1shotykillhead";
+    protected PreparedStatement paInsertar;
+    protected Connection conn = null;
+    protected Statement stmt = null;
+
+    
+    
+    
+    
+    public abstract void conectar();
+}
