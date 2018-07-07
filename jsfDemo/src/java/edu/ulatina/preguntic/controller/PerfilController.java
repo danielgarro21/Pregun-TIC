956461a668675175ecd216572cd05812ea043838
sgtxjosue <sgtxjosue@gmail.com>
2018-07-06 21:05:16 -0600

    actualizado

--- jsfDemo/src/java/edu/ulatina/preguntic/controller/PerfilController.java
+++ jsfDemo/src/java/edu/ulatina/preguntic/controller/PerfilController.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package edu.ulatina.preguntic.controller;
+import edu.ulatina.preguntic.model.Usuario;
+import edu.ulatina.preguntic.service.ServicioUsuario;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -15,5 +19,40 @@ import javax.servlet.http.HttpServletRequest;
 @ManagedBean(name = "perfilController")
 @SessionScoped
 public class PerfilController implements Serializable{
+   private Usuario usuarioLogeado;
+   
+   public String modificarPerfil(){
+       String mostrarPerfil="MODIFICARPERFIL";
+       return mostrarPerfil;
+   }
+   
+   public String mostrarPerfil(){
+       String mostrarPerfil="VERPERFIL";
+       return mostrarPerfil;
+   }
+   public String regresarAlMenu(){
+        String regresar="REGRESARMENUPERFIL"; 
+        return regresar;
+   }
+   public String regresarAlPerfil(){
+        String regresar="REGRESARPERFIL"; 
+        return regresar;
+   }
+
+    public Usuario getUsuarioLogeado() {
+        return usuarioLogeado;
+    }
     
+    public void modificarPerfilDeseado(){
+        ServicioUsuario sUsuarioModificar= new ServicioUsuario();
+        sUsuarioModificar.modificarUsuario(this.usuarioLogeado);
+    }
+
+    public void setUsuarioLogeado(Usuario usuarioLogeado) {
+        
+                this.usuarioLogeado = usuarioLogeado;
+           
+    }
+   
+   
 }
